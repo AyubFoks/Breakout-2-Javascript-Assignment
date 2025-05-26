be4981117d3fb0efae1883fd5f558382d246a563
AyubFoks23 <39079638+AyubFoks@users.noreply.github.com>
2025-05-26 16:50:31 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Question 01
+
 let a = 5; 
 let b = "5";
 
@@ -5,6 +7,8 @@ console.log(a == b); // Output: true, because == checks for value equality and p
 console.log(a === b); // Output: false, because === checks for both value and type equality
 
 
+
+// Question 02
 
 function checkEvenOrOdd(number) {
   if (number % 2 === 0) {
@@ -14,6 +18,8 @@ function checkEvenOrOdd(number) {
   }
     }
 
+
+// Question 03
 
 function gradingSystem(score) {
   if (score >= 90) {
@@ -27,17 +33,22 @@ function gradingSystem(score) {
     }
 
 
+// Question 04
+
 function isPrime(n) {
   if (n <= 1) return false;
   if (n === 2) return true; 
   if (n % 2 === 0) return false; 
 
 
+//Question 05
 
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
+
+// Question 06
 
 let userName = "admin";
 let password = "1234";
@@ -47,6 +58,7 @@ if (userName === "admin" && password === "1234") {
   console.log("Access denied");
 }
 
+// Question 07
 
 function evaluatePassword(password) {
   const isLongEnough = password.length >= 8;
@@ -57,11 +69,15 @@ function evaluatePassword(password) {
 }
 
 
+// Question 08
+
 function isPalindrome(str) {
   const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
   return cleanedStr === cleanedStr.split('').reverse().join('');
 }
 
+
+// Question 09
 
 function checkMultiplesOf3_5(number) {
   if (number % 3 === 0) {
@@ -74,8 +90,11 @@ function checkMultiplesOf3_5(number) {
     }
 
 
+// Question 10
+
 function drawTree(n) {
   for (let i = 1; i <= n; i++) {
     console.log('*'.repeat(i));
   }
 }
+
